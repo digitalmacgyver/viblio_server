00202704939c9883314b397f6e9732fc3da0a569
Andrew Peebles <peebles@video-analytics-inc.com>
2013-04-19 09:29:41 -0700

    simple script to manage the node daemons

---
+++ script/start-stop-node.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# start/stop/restart the node servers
+#
+# usage: start-stop-node [server] start|stop|restart
+#
+# If no server arg, then do them all
+#
+servers='fd fs mq'
+if [ "$1" -a "$2" ]; then
+    cmd="./node/$1/$1-dev.init.d $2"
+    echo $cmd
+    $cmd
+else
+    for server in $servers; do
+	cmd="./node/$server/$server-dev.init.d $1"
+	echo $cmd
+	$cmd
+    done
+fi
