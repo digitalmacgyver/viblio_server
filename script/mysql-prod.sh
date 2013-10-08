c9e2304856e16228df3289a05ff908115a48518c
Andrew Peebles <aqpeeb@gmail.com>
2013-10-07 17:29:51 -0700

    scripts for manual connect to RDS

---
+++ script/mysql-prod.sh
@@ -0,0 +1,1 @@
+mysql -h videos.c9azfz8yt9lz.us-west-2.rds.amazonaws.com -u video_dev --password=video_dev video_dev
---
+++ script/mysql-staging.sh
@@ -0,0 +1,1 @@
+mysql -h testpub.c9azfz8yt9lz.us-west-2.rds.amazonaws.com -u video_dev_1 --password=video_dev_1 video_dev_1
