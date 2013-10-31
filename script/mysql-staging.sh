20c759951fcd2eff03a5a905a0371374499d22fc
Andrew Peebles <peebles@video-analytics-inc.com>
2013-10-31 11:49:45 -0700

    change rds creds, script to add whitelist emails, turn on beta flag

--- script/mysql-staging.sh
+++ script/mysql-staging.sh
@@ -1,1 +1,1 @@
-mysql -h testpub.c9azfz8yt9lz.us-west-2.rds.amazonaws.com -u video_dev_1 --password=video_dev_1 video_dev_1
+mysql -h testpub.c9azfz8yt9lz.us-west-2.rds.amazonaws.com -u web_dev '--password=Yn8U!2Y52Pt#5MEK' video_dev_1
