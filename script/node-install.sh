9c470ac48281812738acc32a1db70c6e7a41449b
Andrew Peebles <aqpeeb@gmail.com>
2013-11-13 17:02:29 -0800

    try to make npm install robust

--- script/node-install.sh
+++ script/node-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
+last_resort="$1"
 servers='mq'
 for server in $servers; do
-    ( cd node/$server; /usr/local/bin/npm install )
+    ( cd node/$server; (/usr/local/bin/npm install||cp -rf $last_resort/node_modules .) )
 done
 
