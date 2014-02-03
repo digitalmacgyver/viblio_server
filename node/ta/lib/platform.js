7e35e77c6891f7f7aead8c4cf674124cd1056b84
Andrew Peebles <peebles@video-analytics-inc.com>
2014-02-03 10:38:34 -0800

    more work on node ta

---
+++ node/ta/lib/platform.js
@@ -0,0 +1,45 @@
+var config = require( '../package.json' );
+var os = require( 'os' );
+var fs = require( 'fs' );
+var path = require( 'path' );
+var mkdirp = require( 'mkdirp' );
+
+module.exports = {
+    // User's HOME
+    home: function() {
+	return process.env.HOME || 
+	    process.env.HOMEPATH || 
+	    process.env.USERPROFILE;
+    },
+    // Where to store local-data
+    appdata: function() {
+	var dir;
+	if ( os.platform() == 'linux' ) {
+	    dir = path.join( module.exports.home(),
+			     '.' + config.name );
+	}
+	else if ( os.platform() == 'darwin' ) {
+	    dir = path.join( module.exports.home(),
+			     'Library', 'Preferences',
+			     config.name );
+	}
+	else {
+	    dir = process.env.LOCALAPPDATA ||
+		process.env.APPDATA;
+	}
+	if ( ! fs.existsSync( dir ) ) {
+	    mkdirp.sync( dir );
+	}
+	return dir;
+    },
+    // TMPDIR
+    tmpdir: function() {
+	return os.tmpDir();
+    },
+    // A string representing this OS
+    platform: function() {
+	return [os.platform(), os.type(),
+		os.arch(), os.release()].join(':');
+    }
+};
+
---
+++ node/ta/pause.js
@@ -0,0 +1,62 @@
+var Uploader = require( './lib/uploader' );
+var async = require( 'async' );
+var util = require( 'util' );
+var config = require( './package.json' );
+
+var filename = '/home/peebles/video-test/test2.mp4';
+// var uuid = '682DC812-05C3-11E3-839F-54DE3DA5649D';
+var uuid = '86FD9216-A8B9-11E2-9637-3B9C97344F04';
+
+var q = async.queue( function( o, cb ) {
+    o.upload( cb );
+}, 4 );
+
+var handler = function( f, err, results ) {
+    if ( err ) {
+	console.log( 'error: ' + err.message );
+	f.retries += 1;
+	if ( f.retries > config.max_retries ) {
+	    console.log( 'max retries exceeded for ' + ( f.fileid || f.filename ) );
+	}
+	else {
+	    setTimeout( function() {
+		q.push( f, function( err, results ) {
+		    handler( f, err, results );
+		});
+	    }, 1000 * f.retries );
+	}
+    }
+    else {
+	console.log( f.fileid + ' is finished.' );
+    }
+}
+
+var f = new Uploader( filename, uuid );
+q.push( f, function( err, results ) {
+    handler( f, err, results );
+});
+
+var at = 0;
+f.on( 'progress', function() {
+    console.log( f.offset, f.length );
+    if ( ++at == 15 ) {
+	console.log( 'pausing ...' );
+	f.pauseUpload().then(
+	    function() {
+		console.log( 'paused! wait 2 seconds, then requeue...' );
+		setTimeout( function() {
+		    q.push( f, function( err, results ) {
+			handler( f, err, results );
+		    });
+		}, 2000 );
+	    },
+	    function( err ) {
+		console.log( 'pause error: ' + err.message );
+	    }
+	);
+    }
+});
+
+q.drain = function() {
+    console.log( 'DONE' );
+}
