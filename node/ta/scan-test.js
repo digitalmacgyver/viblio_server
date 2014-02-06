9c4bb1955ac6a6152510013b6ef09c671fa86187
Andrew Peebles <peebles@video-analytics-inc.com>
2014-02-06 10:49:25 -0800

    added a scan test

---
+++ node/ta/scan-test.js
@@ -0,0 +1,40 @@
+var platform = require( './lib/platform' );
+var Scanner = require( './lib/scan' );
+var async = require( 'async' );
+
+var con = 1;
+var skips = platform.dirskips();
+var dirs = [];
+process.argv.shift();
+process.argv.shift();
+var arg;
+while( arg = process.argv.shift() ) {
+    if ( arg == '-c' )
+	con = parseInt( process.argv.shift() );
+    else if ( arg == '-d' ) 
+	dirs.push( process.argv.shift() );
+    else if ( arg == '-skip' )
+	skips = process.argv.shift();
+}
+
+if ( dirs.length == 0 )
+    dirs = platform.defaultWatchDirs();
+
+var scanner = new Scanner( null, skips );
+scanner.on( 'dir', function( dir ) {
+    console.log( dir );
+});
+var e1 = new Date().getTime();
+async.map( dirs, 
+	   function( dir, cb ) {
+	       scanner.scanForDirs( dir ).then(
+		   function() { cb( null, null ); } );
+	   },
+	   function( err, results ) {
+	       console.log( '... DONE ...' );
+	       var e2 = new Date().getTime();
+	       console.log( 'Took ' + 
+			    ( ( e2 - e1 )/1000 ) + ' seconds' );
+	   }
+	 );
+
