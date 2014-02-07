9dba999d415d2c0f62430042552155162d8c82e7
Jesse Garrison <jesse@gnarwal.com>
2014-02-07 11:51:43 -0800

    Playing with dirs array on status page

--- node/ta/public/app/viewmodels/status.js
+++ node/ta/public/app/viewmodels/status.js
@@ -1,9 +1,19 @@
 define([
         'durandal/app',
-        'viewmodels/ta-header'],
-function(app, taHeader) {
+        'viewmodels/ta-header',
+        'knockout'],
+function(app, taHeader, ko) {
+    var dirs = ko.observableArray([{'name': 'Desktop'}, {'name': 'Shared'}, {'name': 'iPhoto'}, {'name': 'Movies'}]);
+    
+    dirs().forEach(function( d ){
+        d.selected = ko.observable(false);
+        console.log(d);
+    });
+    
     return {
         taHeader: taHeader,
+        dirs: dirs,
+        
 	logout: function() {
 	    app.trigger( 'system:logout' );
 	},
