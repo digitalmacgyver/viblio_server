2dd657f2afa4f0d8687e54bb8531ad2f8088064c
Andrew Peebles <peebles@video-analytics-inc.com>
2014-02-07 19:02:44 -0800

    async version of localStorage

---
+++ node/ta/lib/local-storage.js
@@ -0,0 +1,152 @@
+fs = require( 'fs' );
+path = require( 'path' );
+async = require( 'async' );
+
+var cacheHash = {};
+var cacheArray = [];
+var location;
+var _length = 0;
+var _inited = false;
+
+module.exports = {
+    length: function(callback) {
+	if ( ! _inited ) {
+	    this._readdb(function(err) {
+		if (callback) callback(err,_length);
+	    });
+	}
+	else {
+	    if (callback) callback(null,_length);
+	}
+    },
+
+    _readdb: function(callback) {
+	fs.readdir( location, function( err, files ) {
+	    if ( err ) return callback( err );
+	    cacheHash = {};
+	    cacheArray = [];
+	    async.map( files,
+		       function( file, cb ) {
+			   fs.readFile( path.join( location, file ), 'utf8', function( err, value ) {
+			       if ( err ) return cb(err);
+			       var key = decodeURIComponent( file );
+			       cacheHash[key] = value;
+			       cacheArray.push( key );
+			       cb();
+			   });
+		       },
+		       function( err, results ) {
+			   _length = cacheArray.length;
+			   _inited = true;
+			   callback();
+		       }
+		     );
+	});
+    },
+
+    init: function( root ) {
+	if ( ! fs.existsSync( root ) ) {
+	    fs.mkdirSync( root );
+	}
+	location = root;
+    },
+
+    setItem: function( key, value, callback ) {
+	var filename = path.join( location, encodeURIComponent(key) );
+	fs.writeFile( filename, value, { encoding: 'utf8' }, function(err) {
+	    if ( ! cacheHash[key] ) {
+		cacheHash[key] = value;
+		cacheArray.push( key );
+		_length = cacheArray.length;
+	    }
+	    if (callback) callback(err);
+	});
+    },
+
+    getItem: function( key, callback ) {
+	var filename = path.join( location, encodeURIComponent(key) );
+	if ( cacheHash[key] ) {
+	    if (callback) callback( null, cacheHash[key] );
+	}
+	else {
+	    fs.readFile( filename, 'utf8', function( err, value ) {
+		if ( err ) callback( null, null );
+		else {
+		    if ( value ) {
+			cacheHash[key] = value;
+			cacheArray.push( key );
+			_length = cacheArray.length;
+		    }
+		    if (callback) callback( null, value );
+		}
+	    });
+	}
+    },
+
+    removeItem: function( key, callback ) {
+	var filename = path.join( location, encodeURIComponent(key) );
+	fs.unlink( filename, function(err) {
+	    if ( cacheHash[key] )
+		delete cacheHash[key];
+	    var idx = cacheArray.indexOf( key );
+	    if ( idx != -1 )
+		cacheArray.splice( idx, 1 );
+	    _length = cacheArray.length;
+	    if (callback) callback(err);
+	});
+    },
+
+    clear: function( callback ) {
+	var self = this;
+	async.series([
+	    function(scb) {
+		if ( _inited ) 
+		    scb();
+		else
+		    self._readdb( function( err ) {
+			scb( err );
+		    })
+	    },
+	    function(scb) {
+		var deleted = {};
+		async.map( cacheArray,
+			   function( key, cb ) {
+			       var filename = path.join( location, 
+							 encodeURIComponent(key) );
+			       fs.unlink( filename, function(err) {
+				   delete cacheHash[key];
+				   deleted[key]=true;
+				   cb(err);
+			       });
+			   },
+			   function( err, results ) {
+			       var newArray = [];
+			       cacheArray.forEach( function( key ) {
+				   if ( ! deleted[key] )
+				       newArray.push( key );
+			       });
+			       cacheArray = newArray;
+			       _length = cacheArray.length;
+			       scb(err);
+			   }
+			 );
+	    }
+	], function( err ) {
+	    if (callback) callback(err);
+	});
+    },
+
+    key: function( n, callback ) {
+	if ( ! _inited ) {
+	    this._readdb(function(err) {
+		if (callback) callback( err, cacheArray[n] );
+	    });
+	}
+	else {
+	    if (callback) callback( null, cacheArray[n]  );
+	}
+    }
+
+};
+
+    
