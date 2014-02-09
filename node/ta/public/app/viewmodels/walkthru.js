a5411398d758a54adedc9b15e206e1ecfb3d6e95
Andrew Peebles <peebles@video-analytics-inc.com>
2014-02-09 12:53:56 -0800

    fixed up walkthru for new features, functions

--- node/ta/public/app/viewmodels/walkthru.js
+++ node/ta/public/app/viewmodels/walkthru.js
@@ -12,6 +12,7 @@ define(['durandal/app', 'lib/viblio', 'knockout'], function( app, viblio, ko ) {
     var suggesteddirs = ko.observableArray([]);
 
     var places = ko.observableArray([]);
+    var volumes = ko.observableArray([]);
 
     return {
 	dirs: dirs,
@@ -22,26 +23,30 @@ define(['durandal/app', 'lib/viblio', 'knockout'], function( app, viblio, ko ) {
 	alldirs: alldirs,
 	suggesteddirs: suggesteddirs,
 	places: places,
+	volumes: volumes,
 
 	activate: function() {
 	    dirs.removeAll();
 	    found = {};
-	    app.on( 'mq:scan:dir', function( data ) {
-		dirs.push( data.path );
-		found[ data.path ] = data.label;
+	    app.on( 'mq:scan:file', function( data ) {
+		dirs.push( data.file );
+		if ( ! found[ data.topdir ] )
+		    found[ data.topdir ] = 1;
+		else
+		    found[ data.topdir ] += 1;
 	    });
-	    app.on( 'mq:scan:dir:start', function() {
+	    app.on( 'mq:scan:files:start', function() {
 		dir_scan_done( false );
 	    });
-	    app.on( 'mq:scan:dir:done', function() {
+	    app.on( 'mq:scan:files:done', function() {
 		dir_scan_done( true );
 		viblio.api( '/default_watchdirs' ).then( function( dirs ) {
 		    dirs.forEach( function( dir ) {
-			watchdirs.push({ label: dir.label, path: dir.path, found: (found[dir.path]?true:false) });
+			watchdirs.push({ label: dir.label, path: dir.path, found: found[dir.path] });
 			if ( found[dir.path] )
-			    alldirs.push({ label: dir.label, path: dir.path, found: true });
+			    alldirs.push({ label: dir.label, path: dir.path, found: found[dir.path] });
 			else 
-			    suggesteddirs.push({ label: dir.label, path: dir.path, found: false });
+			    suggesteddirs.push({ label: dir.label, path: dir.path, found: 0 });
 			delete found[ dir.path ];
 		    });
 		    for( var key in found ) {
@@ -52,6 +57,11 @@ define(['durandal/app', 'lib/viblio', 'knockout'], function( app, viblio, ko ) {
 		viblio.api( '/places' ).then( function( dirs ) {
 		    dirs.forEach( function( dir ) {
 			places.push({ label: dir.label, path: dir.path, found: false });
+		    });
+		});
+		viblio.api( '/volumes' ).then( function( dirs ) {
+		    dirs.forEach( function( dir ) {
+			volumes.push({ label: dir.label, path: dir.path, found: false });
 		    });
 		});
 	    });
