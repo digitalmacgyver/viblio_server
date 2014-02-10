e8c3cc65034d1cde537d499c37a7ba5e26f38ae0
Andrew Peebles <peebles@video-analytics-inc.com>
2014-02-10 12:03:41 -0800

    sort dir listings

--- node/ta/app.js
+++ node/ta/app.js
@@ -348,6 +348,9 @@ app.get( '/miller', function( req, res, next ) {
 				   name: s.file,
 				   parent: s.isdir });
 		    });
+		    ret.sort( function( a, b ) {
+			return ((a.name < b.name) ? -1 : ((a.name > b.name) ? 1 : 0));
+		    });
 		    cb( null, ret );
 		});
 	    },
