21c93829eeaa2dd577e508a5f9b2f3d8a7205bad
Andrew Peebles <peebles@video-analytics-inc.com>
2014-05-07 09:46:00 -0700

    a more api-efficent version

--- script/add-geo-info.pl
+++ script/add-geo-info.pl
@@ -6,17 +6,22 @@ use GeoData;
 
 my $c = VA->new;
 
+my $cache = {};
+
 my @media = $c->model( 'RDS::Media' )->all();
 foreach my $media ( @media ) {
+    next if ( $media->geo_address );
     my $lat = $media->lat;
     my $lng = $media->lng;
     if ( $lat && $lng && $lat != 0 && $lng != 0 ) {
-	my $info = GeoData::get_data( $c, $lat, $lng );
+	my $info = $cache->{"$lat$lng"};
+	$info = GeoData::get_data( $c, $lat, $lng ) unless( $info );
 	next unless ( $info->{city} && $info->{address} );
 	print sprintf( "%-20s : %s ( %s, %s )\n", $info->{city}, $info->{address}, $lat, $lng );
 	$media->geo_address( $info->{address} );
 	$media->geo_city( $info->{city} );
 	$media->update;
-	sleep( 1 );
+	sleep( 1 ) unless( $cache->{"$lat$lng"} ); 
+	$cache->{"$lat$lng"} = $info;
     }
 }
